38035fda49282bffad2fe4623b25ab353c720b98
edgartolete <edgartolete@outlook.com>
2023-02-13 03:41:08 +0800

    edit todo feature on progress

--- src/ToDos.jsx
+++ src/ToDos.jsx
@@ -4,13 +4,13 @@ function ToDos ({toDos, setToDos}) {
 
     let [addButtonState, setAddButtonState] = useState(false)
     let [toDoName, setToDoName] = useState("")
+    let [disabledToken, toggleDisabledToken] = useState(true);
     function addButtonToggle(){
         setAddButtonState(addButtonState = !addButtonState)
     }
 
     function addTask(){
         if(toDoName != ""){
-            console.log(toDoName);
             let newId = 1;
             if(toDos.length > 0){
                 newId = toDos.length;
@@ -24,7 +24,7 @@ function ToDos ({toDos, setToDos}) {
     }
 
     function updateToDo (){
-        console.log("test")
+        
     }
     return (
         <div className='all-todos'>
@@ -33,12 +33,16 @@ function ToDos ({toDos, setToDos}) {
                 <button className='todo-add-button' onClick={addButtonToggle}>Add</button> : 
                     <div className='todo-add-input'>
                         <input type="text" placeholder='Enter new task.' onChange={e => setToDoName(e.target.value + "")}></input>
-                        <div className='add-action-button'><button onClick={addTask} className="task-button">Add</button><button className="task-button" onClick={addButtonToggle}>Cancel</button>
+                        <div className='task-buttons'><button onClick={addTask} className="task-button">Add</button><button className="task-button" onClick={addButtonToggle}>Cancel</button>
                     </div>
             </div> } 
             {toDos.map(todo => (todo.status == "new")?
                     <div className='todo-card' key={todo.id} >
-                        <input type="text" value={todo.name} onChange={updateToDo} disabled/>
+                        <input type="text" value={todo.name} disabled={disabledToken ? "disabled" : ""}/>
+                        <div className='task-buttons'>
+                            <button className='task-button'>Edit</button>
+                            <button className='task-button'>Delete</button>
+                        </div>
                     </div>
                     : ""
                 )}
